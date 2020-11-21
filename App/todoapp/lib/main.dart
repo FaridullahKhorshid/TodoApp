6bda0cbbc4f84b5392862e1133da2ad0428c591a
Faridullah Khorshid <faridullah.khorshid@gmail.com>
2020-11-21 19:32:28 +0100

    navbar update

--- App/todoapp/lib/main.dart
+++ App/todoapp/lib/main.dart
@@ -1,20 +1,20 @@
+import 'package:todoapp/fab_with_icons.dart';
+import 'package:todoapp/fab_bottom_app_bar.dart';
+import 'package:todoapp/layout.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo App',
-      theme: ThemeData(
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Todo App'),
+      home: new MyHomePage(title: 'BottomAppBar with FAB'),
     );
   }
 }
@@ -25,87 +25,76 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  String _lastSelected = 'TAB: 0';
+
+  void _selectedTab(int index) {
+    setState(() {
+      _lastSelected = 'TAB: $index';
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: DefaultTabController(
-        length: 5,
-        child: new Scaffold(
-          appBar: AppBar(
-            title: Text('Todo'),
-          ),
-          body: TabBarView(
-            children: getTabViews(), // get all tab view
-          ),
-          bottomNavigationBar: new Container(
-            height: 60,
-            child: new TabBar(
-              tabs: getTabs(), // get the nav items
-              labelColor: Colors.cyan,
-              unselectedLabelColor: Colors.grey,
-            ),
-          ),
-          backgroundColor: Colors.cyan[50],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Text(
+          _lastSelected,
+          style: TextStyle(fontSize: 32.0),
         ),
+      ),
+      bottomNavigationBar: FABBottomAppBar(
+        centerItemText: 'A',
+        color: Colors.grey,
+        selectedColor: Colors.red,
+        notchedShape: CircularNotchedRectangle(),
+        onTabSelected: _selectedTab,
+        items: [
+          FABBottomAppBarItem(iconData: Icons.menu, text: 'This'),
+          FABBottomAppBarItem(iconData: Icons.layers, text: 'Is'),
+          FABBottomAppBarItem(iconData: Icons.dashboard, text: 'Bottom'),
+          FABBottomAppBarItem(iconData: Icons.info, text: 'Bar'),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your onPressed code here!
+        },
+        child: Icon(
+          Icons.add,
+          size: 50,
+        ),
+        backgroundColor: Colors.green,
       ),
     );
   }
 
-  getTabViews() {
-    return [
-      new Container(
-        color: Colors.yellow,
-      ),
-      new Container(
-        color: Colors.orange,
-      ),
-      new Container(
-        color: Colors.lightGreen,
-      ),
-      new Container(
-        color: Colors.red,
-      ),
-      new Container(
-        color: Colors.blue,
-      ),
-    ];
-  }
-
-  getTabs() {
-    return [
-      Tab(
-        icon: new Icon(
-          Icons.home,
-          size: 30,
-        ),
-      ),
-      Tab(
-        icon: new Icon(
-          Icons.rss_feed,
-          size: 30,
-        ),
-      ),
-      Tab(
-        icon: new Container(
-          child: Icon(Icons.add),
-        ),
-      ),
-      Tab(
-        icon: new Icon(
-          Icons.perm_identity,
-          size: 30,
-        ),
-      ),
-      Tab(
-        icon: new Icon(
-          Icons.settings,
-          size: 30,
-        ),
-      )
-    ];
-  }
+  // Widget _buildFab(BuildContext context) {
+  //   final icons = [Icons.sms, Icons.mail, Icons.phone];
+  //   return AnchoredOverlay(
+  //     showOverlay: true,
+  //     overlayBuilder: (context, offset) {
+  //       return CenterAbout(
+  //         position: Offset(offset.dx, offset.dy - icons.length * 35.0),
+  //         child: FabWithIcons(
+  //           icons: icons,
+  //           onIconTapped: _selectedFab,
+  //         ),
+  //       );
+  //     },
+  //     child: FloatingActionButton(
+  //       onPressed: () {},
+  //       tooltip: 'Increment',
+  //       child: Icon(Icons.add),
+  //       elevation: 2.0,
+  //     ),
+  //   );
+  // }
 }
